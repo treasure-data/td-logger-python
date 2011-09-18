b2dd8a7ec62dcfc3d1e652b160228ddfd1535e50
Kazuki Ohta <kazuki.ohta@gmail.com>
2011-09-18 23:26:21 +0900

    verbose logging in testcase

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -7,8 +7,8 @@ class TestHandler(unittest.TestCase):
     def testLogging(self):
         logging.basicConfig(level=logging.INFO)
         l = logger.TreasureDataLogger('td_logger.test')
-        l.addHandler(handler.TreasureDataHandler())
+        l.addHandler(handler.TreasureDataHandler(verbose=True))
 
         js = { "semicolon" : ";", "at" : "@" }
-        for i in range(0, 100):
+        for i in range(0, 5):
             l.info(js)
