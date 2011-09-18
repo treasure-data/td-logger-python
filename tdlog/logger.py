553b13a0db47d5d7186e94971543e5c2862dffe9
Kazuki Ohta <kazuki.ohta@gmail.com>
2011-09-19 00:08:21 +0900

    modify for discoverable initialization

--- tdlog/logger.py
+++ tdlog/logger.py
@@ -1,77 +1,111 @@
 import logging
-import sys
 import os
+import sys, urllib
+import msgpack
+import socket
+import time
 
-from socket import gethostname
-from datetime import datetime
+class TreasureDataLogRecordFormatter:
+    def __init__(self, db, table):
+        self.hostname = socket.gethostname()
+        self.db = db
+        self.table = table
 
-__all__ = ['TreasureDataLogRecord', 'TreasureDataLogger']
-
-class TreasureDataLogRecord(logging.LogRecord):
-    def __init__(self, name, level, fn, lno, msg, args,exc_info, func, extra=None):
-        logging.LogRecord.__init__(self, name, level, fn, lno, msg, args, exc_info, func)
-
-        self.username = _current_user()
-        self.funcname = _calling_func_name()
-
-        self._raw = {
-            'name' : name,
-            'level' : _level_to_str(level),
-            'file' : fn,
-            'line_no' : lno,
-            'msg' : msg,
-            'args' : list(args),
-            'exc_info' : exc_info,
-            'user' : self.username,
-            'funcname' : self.funcname,
-            'host' : gethostname()
+    def format(self, record):
+        data = {
+          'host' : self.hostname,
+          'name' : record.name,
+          'module' : record.module,
+          'lineno' : record.lineno,
+          'levelno' : record.levelno,
+          'levelname' : record.levelname,
+          'filename' : record.filename,
+          'funcname' : record.funcName,
+          'exc_info' : record.exc_info,
         }
+        if 'exc_info' in data and data['exc_info']:
+            data['exc_info'] = self.formatException(data['exc_info'])
 
-class TreasureDataLogger(logging.getLoggerClass()):
-    def makeRecord(self, *args, **kwargs):
-        return TreasureDataLogRecord(*args, **kwargs)
+        tag = "td.%s.%s" % (self.db, self.table)
+        packet = [ tag, long(time.time()), data ]
+        return packet
 
-def _level_to_str(level):
-    """ Convert a numeric logging level to string representation  """
-    if logging.DEBUG == level:
-        return u'debug'
-    elif logging.INFO == level:
-        return u'info'
-    elif logging.WARNING == level:
-        return u'warning'
-    elif logging.ERROR == level:
-        return u'error'
-    elif logging.CRITICAL  == level:
-        return u'critical'
-    else:
-        return u'undefined'
+class TreasureDataHandler(logging.Handler):
+    '''
+    Logging Handler for td-agent.
+    '''
+    def __init__(self,
+           host='127.0.0.1',
+           port=24224,
+           db='log',
+           table='default',
+           bufmax=1*1024*1024,
+           verbose=False):
 
-def _current_user():
-    """ Get the name of the os user running this app """
-    import pwd, os
-    try:
-        return pwd.getpwuid(os.getuid()).pw_name
-    except KeyError:
-        return "(unknown)"
+        self.host = host
+        self.port = port
+        self.db = db
+        self.table = table
+        self.bufmax = bufmax
+        self.verbose = verbose
 
-def _calling_func_name():
-    return _calling_frame().f_code.co_name
+        self.pendings = None
+        self.packer = msgpack.Packer()
+        self.fmt = TreasureDataLogRecordFormatter(self.db, self.table)
+        try:
+            self.socket = self._connect()
+        except:
+            # will be retried in emit()
+            self.socket = None
 
-def _calling_frame():
-    f = sys._getframe()
+        logging.Handler.__init__(self)
 
-    while True:
-        if _is_user_source_file(f.f_code.co_filename):
-            return f
-        f= f.f_back
+    def __del__(self):
+        self._close()
 
-def _is_user_source_file(filename):
-    return os.path.normcase(filename) not in (_srcfile, logging._srcfile)
+    def emit(self, record):
+        if record.levelno < self.level: return
 
-def _current_source_file():
-    if __file__[-4:].lower() in ['.pyc', '.pyo']:
-        return __file__[:-4] + '.py'
-    else:
-        return __file__
- 
-_srcfile = os.path.normcase(_current_source_file())
+        packet = self.fmt.format(record)
+        if self.verbose:
+            print packet
+        packet = self.packer.pack(packet)
+        
+        if self.pendings:
+            self.pendings.append(data)
+            data = self.pendings
+        try:
+            # reconnect if possible
+            self._reconnect()
+
+            # send message
+            total = len(data)
+            nsent = 0
+            while nsent < total:
+                n = self.socket.send(data[nsent:])
+                if n <= 0:
+                    raise RuntimeError("socket connection broken")
+                nsent += n
+
+            # send finished
+            self.pendings = None
+        except:
+            # clear buffer if it exceeds max bufer size
+            if self.pendings and (len(self.pendings) > self.bufmax):
+                self.pendings = None
+            # close socket
+            self._close()
+
+    def _reconnect(self):
+        if not self.socket:
+            self._connect()
+
+    def _connect(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((self.host, self.port))
+        return sock
+
+    def _close(self):
+        if self.socket:
+            self.socket.close()
+        self.socket = None
--- tests/test_handler.py
+++ tests/test_handler.py
@@ -1,13 +1,13 @@
 import unittest
 import logging
 
-from tdlog import handler, logger
+from tdlog import logger
 
 class TestHandler(unittest.TestCase):
     def testLogging(self):
         logging.basicConfig(level=logging.INFO)
-        l = logger.TreasureDataLogger('td_logger.test')
-        l.addHandler(handler.TreasureDataHandler(verbose=True))
+        l = logging.getLogger('td_logger.test')
+        l.addHandler(logger.TreasureDataHandler(verbose=True))
 
         js = { "semicolon" : ";", "at" : "@" }
         for i in range(0, 5):
