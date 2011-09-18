4981d19d5c34597114e0203576a59ce3c83bf905
Kazuki Ohta <kazuki.ohta@gmail.com>
2011-09-18 22:37:53 +0900

    add .gitignore

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   version='0.1',
   package_dir={'': 'src'},
   packages=[''],
-  install_requires=['msgpack-python']
+  install_requires=['msgpack-python'],
   author='Kazuki Ohta',
   author_email='k@treasure-data.com',
   url='https://github.com/treasure-data/td-logger-python',
