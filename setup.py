2a06cc448e308e98c7107951df7d43d9f62d319f
Kazuki Ohta <kazuki.ohta@gmail.com>
2011-09-19 01:43:10 +0900

    bump version 0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
   name='td-logger',
-  version='0.1',
+  version='0.2',
   package_dir={'tdlog': 'tdlog'},
   packages=['tdlog'],
   install_requires=['msgpack-python'],
